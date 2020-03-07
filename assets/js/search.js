6b6a061e06f3c4f6e78daaa21baa4e8b3c86c11b
Ted Yuen <tedyuen.goo@gmail.com>
2020-03-07 14:17:09 +0800

    init

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class","2048":"Method"},"rows":[{"id":0,"kind":128,"name":"UrlSearch","url":"classes/urlsearch.html","classes":"tsd-kind-class"},{"id":1,"kind":2048,"name":"parse","url":"classes/urlsearch.html#parse","classes":"tsd-kind-method tsd-parent-kind-class","parent":"UrlSearch"}]};
